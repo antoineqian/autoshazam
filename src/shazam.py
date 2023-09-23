491b1fd12cf44de9101e1f690290180e82ad12ad
Antoine Qian <antoine.qian@protonmail.com>
2023-09-23 18:42:27 +0200

    Move function out of main

--- src/shazam.py
+++ src/shazam.py
@@ -17,24 +17,25 @@ parser.add_argument("-i", "--interval", help='A call to Shazam will be performed
 args = parser.parse_args()
 filename, interval = args.filename, args.interval * 60 * 1000
 
-async def main():
+async def do_shazam(shazam, part, i):
+    ret = await shazam.recognize_song(part)
+    if ret['matches']:
+        return i, ret['track'] 
+
+async def shazam_all(filename):
     shazam = Shazam()
     seg = AudioSegment.from_file(filename)
     dur = seg.duration_seconds
-    
+
     iters = ceil(dur * 1000 / interval)
 
-    async def shazam_at(i):
-        part = seg[i*interval:(i+1)*interval]
-        ret = await shazam.recognize_song(part)
-        if ret['matches']:
-            return i, ret['track']        
-
-    coros = [shazam_at(i) for i in range(iters)]
+    coros = [do_shazam(shazam, seg[i*interval:(i+1)*interval], i) for i in range(iters)]
     results = await asyncio.gather(*coros)
     for i, track in sorted(results):
         print(f"The song was recognized to be {track['title']} :  {track['subtitle']} at {i*interval/60/1000} min")
 
+async def main():
+    await shazam_all(filename)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
