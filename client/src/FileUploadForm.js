98d0dab8fc239b50179873f1121ee720308301c2
Antoine Qian <antoine.qian@protonmail.com>
2024-01-05 12:04:37 +0100

    Foundation for yt_dlp and processFolder

--- client/src/FileUploadForm.js
+++ client/src/FileUploadForm.js
@@ -2,31 +2,41 @@ import React, { useState } from 'react';
 import axios from 'axios'
 import LoadingButton from './LoadingButton'
 
-const FileUploadForm = ({setTracks}) => {
+const FileUploadForm = ({ setTracks }) => {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [fileList, setFileList] = useState([]);
   const [intervalValue, setIntervalValue] = useState(3);
   const [isValid, setIsValid] = useState(true);
   const [loading, setLoading] = useState(false);
   const [url, setUrl] = useState('');
 
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedFile(file);
+    const fileList = event.target.files;
+
+    if (fileList.length === 1) {
+      // Single file selected
+      const file = fileList[0];
+      setSelectedFile(file);
+    } else {
+      // Folder or multiple files selected
+      const folderFiles = Array.from(fileList);
+      setFileList(folderFiles);
+    }
   };
 
   const handleIntervalChange = (event) => {
-    const value = event.target.value; 
+    const value = event.target.value;
 
     setIntervalValue(value);
     // Check if the entered value is a valid interval
     setIsValid(/^\d+$/.test(value));
   };
-  
+
   const handleUrlChange = (event) => {
     const value = event.target.value;
     setUrl(value);
   };
-  
+
   const [inputReset, setInputReset] = useState('')
   // Assigining a new key will force re-render
   const resetInput = () => {
@@ -36,29 +46,49 @@ const FileUploadForm = ({setTracks}) => {
 
   const handleUpload = async () => {
     // Handle file upload logic here
-    if (!selectedFile ){
+    if (!selectedFile) {
       console.log("No file selected")
     }
-    if(!isValid){
+    if (!isValid) {
       console.log("Invalid input")
     }
     setLoading(true);
+    if (fileList.length > 0) {
+      console.log("Uploading folder:", fileList.length)
+      const formData = new FormData();
+      fileList.forEach((file, index) => {
+        formData.append(`files`, file);
+      });
+      formData.append('interval', parseInt(intervalValue));
+      const response = await axios.post(
+        `http://localhost:8000/processFolder`,
+        formData,
+        { headers: { 'Content-Type': 'multipart/form-data' } }
+      )
+      if (response.status === 200) {
+        console.log('Detection done')
+        console.log(response)
+      }
+      setTracks(response.data)
+    }
+
     if (selectedFile) {
       console.log('Uploading file:', selectedFile);
       const formData = new FormData();
       formData.append('file', selectedFile);
       formData.append('interval', parseInt(intervalValue));
       const response = await axios.post(
-          `http://localhost:8000/processFile`,
-          formData,
-          { headers: {'Content-Type': 'multipart/form-data'}}
+        `http://localhost:8000/processFile`,
+        formData,
+        { headers: { 'Content-Type': 'multipart/form-data' } }
       )
-    
+
       if (response.status === 200) {
         console.log('Detection done')
         console.log(response)
       }
-      setTracks(response.data)}
+      setTracks(response.data)
+    }
     else {
       const formData = new FormData();
       formData.append('url', url);
@@ -66,7 +96,7 @@ const FileUploadForm = ({setTracks}) => {
       const response = await axios.post(
         `http://localhost:8000/processUrl`,
         formData,
-        { headers: {'Content-Type': 'multipart/form-data'}}
+        { headers: { 'Content-Type': 'multipart/form-data' } }
       )
     }
     setLoading(false);
@@ -79,28 +109,30 @@ const FileUploadForm = ({setTracks}) => {
     <div className="file-upload-form">
       <h2>Upload your file here or paste a link</h2>
       <form onSubmit={handleUpload}>
-      <input
-        key={inputReset}
-        type="file"
-        accept=".mp3, .wav, audio/*"  // Adjust file types as needed
-        onChange={handleFileChange}
-      />
-      <input 
-        type='url'
-        value={url}
-        onChange={handleUrlChange}
-        placeholder='https://soundcloud.com/petervanhoesen/sync-live-at-bassiani-september-2022'
-        required={false}
-      />
-      <input
-        type="text"
-        id="intervalInput"
-        value={intervalValue}
-        onChange={handleIntervalChange}
-        placeholder="Enter an interval"
-        style={{ borderColor: isValid ? 'inherit' : 'red' }}
-      />
-      <LoadingButton text="Submit" onSubmit={handleUpload} loading={loading} />
+        <input
+          key={inputReset}
+          type="file"
+          accept=".mp3, .wav, audio/*"  // Adjust file types as needed
+          onChange={handleFileChange}
+          directory=""  // Enable directory selection
+          webkitdirectory=""  // Enable directory selection for WebKit browsers (like Chrome)
+        />
+        <input
+          type='url'
+          value={url}
+          onChange={handleUrlChange}
+          placeholder='https://soundcloud.com/petervanhoesen/sync-live-at-bassiani-september-2022'
+          required={false}
+        />
+        <input
+          type="text"
+          id="intervalInput"
+          value={intervalValue}
+          onChange={handleIntervalChange}
+          placeholder="Enter an interval"
+          style={{ borderColor: isValid ? 'inherit' : 'red' }}
+        />
+        <LoadingButton text="Submit" onSubmit={handleUpload} loading={loading} />
       </form>
     </div>
   );
--- server/src/app/main.py
+++ server/src/app/main.py
@@ -3,8 +3,9 @@ from fastapi import FastAPI, Form, WebSocket
 from fastapi import File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+from yt_dlp import YoutubeDL, postprocessor
 
-from ..infra.shazam import shazam_file, process_url
+from ..infra.shazam import shazam_file
 
 app = FastAPI()
 
@@ -15,8 +16,26 @@ app.add_middleware(
  allow_methods=["*"],
  allow_headers=["*"],
 )
-from yt_dlp import YoutubeDL, postprocessor
 
+@app.post("/processFolder")
+async def processFolder(files: list[UploadFile] = File(...), interval: int=Form(...)):
+    for file in files:
+        print(f"Processing file {file.filename} with interval {interval}")
+
+    # print(f"Processing folder {folder.filename} with interval {interval}")
+    # try:
+    #     contents = folder.file.read()
+    #     file_location = os.path.join('./storage', folder.filename)
+
+    #     with open(file_location, 'wb') as f:
+    #         f.write(contents)
+    #     results = await shazam_file(file_location, interval)
+    #     return results
+    # except Exception as e:
+    #     print(e)
+    #     return {"message": "There was an error uploading the file", "e": e}
+    # finally:
+    #     folder.file.close()
 
 @app.post("/processFile")
 async def processFile(file: UploadFile=File(...), interval: int=Form(...)):
@@ -28,29 +47,28 @@ async def processFile(file: UploadFile=File(...), interval: int=Form(...)):
         with open(file_location, 'wb') as f:
             f.write(contents)
         results = await shazam_file(file_location, interval)
-
+        return results
     except Exception as e:
         print(e)
         return {"message": "There was an error uploading the file", "e": e}
     finally:
         file.file.close()
-    return results
 
 
 
-@app.post("/processUrl")
-async def processUrl(url: str=Form(...), interval:int=Form(...)):
-    print(f"Processing url {url} with {interval} seconds")
-    process_url(url, interval)
-    # file_location = './storage/downloaded_file.%(ext)s'
-    # ydl_opts = {
-    #     'outtmpl': file_location,
-    #     'progress_hooks': [print_location],
-    # }
-    # with YoutubeDL(ydl_opts) as ydl:
+# @app.post("/processUrl")
+# async def processUrl(url: str=Form(...), interval:int=Form(...)):
+#     print(f"Processing url {url} with {interval} seconds")
+#     process_url(url, interval)
+#     # file_location = './storage/downloaded_file.%(ext)s'
+#     # ydl_opts = {
+#     #     'outtmpl': file_location,
+#     #     'progress_hooks': [print_location],
+#     # }
+#     # with YoutubeDL(ydl_opts) as ydl:
         
-    #     ydl.add_post_processor(ShazamProcessor(), when='post_process')
-    #     ydl.download(url)
-    return await shazam_file(downloaded_file_path, interval)
+#     #     ydl.add_post_processor(ShazamProcessor(), when='post_process')
+#     #     ydl.download(url)
+#     return await shazam_file(downloaded_file_path, interval)
 
 
