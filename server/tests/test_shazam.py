c20c9a3f4d139b08c3d1a1736f689ab57944937f
Antoine Qian <antoine.qian@protonmail.com>
2024-02-10 21:08:29 +0100

    Init ci/cd

---
+++ server/tests/test_shazam.py
@@ -0,0 +1,20 @@
+from unittest.mock import AsyncMock
+import pytest
+
+from server.src.infra.shazam import shazam_file
+
+pytest.mark.asyncio 
+
+
+def test_processFolder():
+    shazam_file = AsyncMock()
+    pass
+
+
+def test_processUrl():
+    pass
+
+async def test_shazam_file():
+    result = await shazam_file("just_noise.mp3", 1)
+    assert type(result) == list
+    assert len(result) == 0
